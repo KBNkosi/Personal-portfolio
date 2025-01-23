acfde90a9ca04a5fab021b1de7e75f53d7c945fc
Nkosikenny <kennybhekumuzi@gmail.com>
2025-01-23 13:41:12 +0200

    Refactor Home component layout and implement Portfolio section with project images and buttons for demo and code links

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800' >
 
       <div className='max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4'>
-        <div className='flex flex-col justify-center h-full'>
+        <div className='flex flex-col justify-center h-[40%]'>
         <h2 className='text-4xl sm:text-7xl font-bold text-white'>I am a IT Graduate</h2>
         <p className='text-gray-300 py-4 max-w-md'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam magni,
